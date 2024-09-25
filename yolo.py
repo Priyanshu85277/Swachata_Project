d5108240a2dc95853096467834d2c0a05988236d
Priyanshu85277 <166702264+Priyanshu85277@users.noreply.github.com>
2024-09-26 00:02:44 +0530

    yolo.py

--- yolo.py
+++ yolo.py
@@ -3,5 +3,5 @@ model = YOLO('best.pt')  # load a pretrained model (recommended for training)
 
 
 # Run inference with the YOLOv8n model on the 'bus.jpg' image
-results = model.predict(source='Test/OIP (5).jpeg',conf=0.25,save=True)
+results = model.predict(source='Test/OIP (4).jpeg',conf=0.25,save=True)
 print(results)
